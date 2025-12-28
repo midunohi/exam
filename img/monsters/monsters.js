4918dfa2da3a078be9be5981625e0eb0dc47c351
midunohi <mznh.101.ch@gmail.com>
2025-12-28 20:19:33 +0900

    monsters

---
+++ img/monsters/monsters.js
@@ -0,0 +1,6 @@
+export const MONSTERS = [
+  { id: "ash_wyrm",    ja: "アッシュ・ワーム" },
+  { id: "dust_lizard", ja: "ダスト・リザード" },
+  { id: "ember_golem", ja: "エンバー・ゴーレム" },
+  { id: "night_siren", ja: "ナイト・サイレン" }
+];
