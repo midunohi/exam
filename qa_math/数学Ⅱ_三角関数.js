050ce4d190e2e3d62566c9d0d830be83f3734318
midunohi <mznh.101.ch@gmail.com>
2026-06-02 10:44:16 +0900

    .md

--- qa_math/数学Ⅱ_三角関数.js
+++ qa_math/数学Ⅱ_三角関数.js
@@ -1,16 +1,16 @@
 // 数学Ⅱ: 三角関数
 const qa_数学Ⅱ_三角関数 = [
-  ["\\(\\sin^2 x+\\cos^2 xは？\\)","\\(1\\)"],
-  ["\\(1+\\tan^2 xは？\\)","\\(\\frac{1}{\\cos^2 x}\\)"],
-  ["\\(\\sin(\\alpha+\\beta)は？\\)","\\(\\sin\\alpha\\cos\\beta+\\cos\\alpha\\sin\\beta\\)"],
-  ["\\(\\cos(\\alpha+\\beta)は？\\)","\\(\\cos\\alpha\\cos\\beta-\\sin\\alpha\\sin\\beta\\)"],
-  ["\\(\\sin2xは？\\)","\\(2\\sin x\\cos x\\)"],
-  ["\\(\\cos2xは？\\)","\\(\\cos^2 x-\\sin^2 x\\)"],
-  ["\\(\\tan2xは？\\)","\\(\\frac{2\\tan x}{1-\\tan^2 x}\\)"],
-  ["\\(\\sin\\frac{\\pi}{6}は？\\)","\\(\\frac{1}{2}\\)"],
-  ["\\(\\cos\\frac{\\pi}{3}は？\\)","\\(\\frac{1}{2}\\)"],
-  ["\\(\\tan\\frac{\\pi}{4}は？\\)","\\(1\\)"],
-  ["\\(\\sin\\frac{\\pi}{3}は？\\)","\\(\\frac{\\sqrt3}{2}\\)"],
-  ["\\(\\cos\\frac{\\pi}{6}は？\\)","\\(\\frac{\\sqrt3}{2}\\)"],
-  ["\\(\\tan\\frac{\\pi}{6}は？\\)","\\(\\frac{1}{\\sqrt3}\\)"]
+  ["\\(\\sin^2 x+\\cos^2 x\\) =", "\\(1\\)"],
+  ["\\(1+\\tan^2 x\\) =", "\\(\\frac{1}{\\cos^2 x}\\)"],
+  ["\\(\\sin(\\alpha+\\beta)\\) =", "\\(\\sin\\alpha\\cos\\beta+\\cos\\alpha\\sin\\beta\\)"],
+  ["\\(\\cos(\\alpha+\\beta)\\) =", "\\(\\cos\\alpha\\cos\\beta-\\sin\\alpha\\sin\\beta\\)"],
+  ["\\(\\sin2x\\) =", "\\(2\\sin x\\cos x\\)"],
+  ["\\(\\cos2x\\) =", "\\(\\cos^2 x-\\sin^2 x\\)"],
+  ["\\(\\tan2x\\) =", "\\(\\frac{2\\tan x}{1-\\tan^2 x}\\)"],
+  ["\\(\\sin\\frac{\\pi}{6}\\) =", "\\(\\frac{1}{2}\\)"],
+  ["\\(\\cos\\frac{\\pi}{3}\\) =", "\\(\\frac{1}{2}\\)"],
+  ["\\(\\tan\\frac{\\pi}{4}\\) =", "\\(1\\)"],
+  ["\\(\\sin\\frac{\\pi}{3}\\) =", "\\(\\frac{\\sqrt3}{2}\\)"],
+  ["\\(\\cos\\frac{\\pi}{6}\\) =", "\\(\\frac{\\sqrt3}{2}\\)"],
+  ["\\(\\tan\\frac{\\pi}{6}\\) =", "\\(\\frac{1}{\\sqrt3}\\)"]
 ];
