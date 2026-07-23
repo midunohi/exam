cb08180cb5c291e62608431a11b634866e705cfe
midunohi <mznh.101.ch@gmail.com>
2026-07-23 18:13:06 +0900

    .js

--- qa_tex/数学/数学Ⅰ_数と式_集合と命題.js
+++
@@ -1,8 +0,0 @@
-const qa_数学Ⅰ_数と式_集合と命題 = [
-  ["ド・モルガンの法則 \\(\\overline{A\\cup B}=\\)", "\\(\\overline{A}\\cap\\overline{B}\\)"],
-  ["命題 \\(p\\Rightarrow q\\) の逆は？", "\\(q\\Rightarrow p\\)"],
-  ["命題 \\(p\\Rightarrow q\\) の裏は？", "\\(\\lnot p\\Rightarrow \\lnot q\\)"],
-  ["命題 \\(p\\Rightarrow q\\) の対偶は？", "\\(\\lnot q\\Rightarrow \\lnot p\\)"],
-  ["\\(p\\) は \\(q\\) であるための十分条件とは？", "\\(p\\Rightarrow q\\) が成り立つこと"],
-  ["\\(p\\) は \\(q\\) であるための必要条件とは？", "\\(q\\Rightarrow p\\) が成り立つこと"]
-];
